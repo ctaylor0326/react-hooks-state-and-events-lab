4c1fe17e7f4896e89e9dfca4a0cee0a9794426c6
CahenTaylor <cahentaylor@gmail.com>
2023-04-21 10:14:49 -0600

    dark mode, add to cart working

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+
+  const [isInCart, setIsInCart] = useState(false)
+  const toggleIsInCart = () => {
+    setIsInCart(!isInCart)
+  }
   return (
-    <li className="">
+    <li className={isInCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={toggleIsInCart} className="add">{isInCart ? "Remove From Cart" : "Add to Cart"} </button>
     </li>
   );
 }
